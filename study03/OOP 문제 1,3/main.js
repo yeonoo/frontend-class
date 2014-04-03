92f7ac7df4f26642dcdc19748ca7eae82cc2537b
yeonoo <ywoo818@gmail.com>
2014-04-03 12:53:03 +0900

    수정

--- study03/OOP 문제 1,3/main.js
+++ study03/OOP 문제 1,3/main.js
@@ -1,16 +1,29 @@
-var myPhone = new IPhone({ model:'IPhone5', release:2012, color:white});
+var myPhone1 = new IPhone({ model:'IPhone5', release:2012, color:white});
+var myPhone2 = new Android({ model:'GalaxyS5', release:2014, color:gold});
 
-console.log(myPhone.color);
-console.log(myPhone.model);
+console.log(myPhone1.color);
+console.log(myPhone1.model);
 
-myPhone.setNumber( '01122020303');
-myPhone.call();
+console.log(myPhone2.color);
+console.log(myPhone2.model);
 
+myPhone1.setNumber( '01122020303');
+myPhone1.call();
 
-myPhone.boot();
-myPhone.call();
-myPhone.message();
-myPhone.camera();
-myPhone.callSiri();
-myPhone.startFaceTime();
-myPhone.openPhotoStream();
+myPhone2.setNumber( '01122020304');
+myPhone2.call();
+
+myPhone1.boot();
+myPhone1.call();
+myPhone1.message();
+myPhone1.camera();
+myPhone1.callSiri();
+myPhone1.startFaceTime();
+myPhone1.openPhotoStream();
+
+myPhone2.boot();
+myPhone2.call();
+myPhone2.message();
+myPhone2.camera();
+myPhone2.googleMarket();
+myPhone2.sNote();
