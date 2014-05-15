27ad39bd89863be3d644c865a892af8079f96251
yeonoo <ywoo818@gmail.com>
2014-05-15 17:15:17 +0900

    kireini
    kireini

--- app.js
+++ app.js
@@ -1,46 +1,40 @@
-var API_URL = "http://apis.daum.net/search/web?q={query}&apikey=81f98f832fab2e5a037d48bf0f92e4402f1a38e0&output=JSON&callback={callback}";
-var num = 0;
-var items;
-var searchR;
-$(document).ready(function(){
-    $('#search').click(function(){
-        $("#result").empty();
-        $("#pageno").val("1");
-        var query = $('#query').val();
-        var url = API_URL.replace('{query}',query).replace('{callback}','?');
-        $.ajax(url,{
-            cache : true,
-            dataType : 'jsonp',
-            complete  :function(jqXHR, status){
-                var data = jqXHR.responseJSON;
-                items = data.channel.item;
-                for(var i = num; i < (num+5); i++) {
-                var searchR="<li><a href='"+items[i].link+"'>"+items[i].title+"</li>";
- 			    $('#result').append(searchR);
-                }
-                num = num+5;
-            }
-        });
-        return false;
-    });
+var API_URL = "http://apis.daum.net/search/web?q= \
+{query}&apikey=81f98f832fab2e5a037d48bf0f92e4402f1a38e0&\
+output=json&callback={callback}&pageno={pageno}";
 
-    $('#moreBtn').click(function(){
-        $('#pageno').val(parseInt($('#pageno').val())+1);
-        var query = $('#query').val();
-        var url = API_URL.replace('{query}',query).replace('{callback}','?');
-        $.ajax(url,{
-            cache : true,
-            dataType : 'jsonp',
-            complete  :function(jqXHR, status){
-                var data = jqXHR.responseJSON;
-                items = data.channel.item;
-                for(var i = num; i < (num+5); i++) {
-                var searchR="<li><a href='"+items[i].link+"'>"+items[i].title+"</li>";
- 			    $('#result').append(searchR);
-                }
-                num = num+5;
-            }
-        });
-        return false;
-    });
+
+var pageno = 1;
+$('#searchForm').on("submit",function(event){
+    console.log(1);
+    $("#result").empty();
+    $("#pageno").val("1");
+	pageView();
+    return false;
 });
+
+$('#moreBtn').on("click", function(event){
+    pageno++;
+    $('#pageno').val(pageno);
+    pageView();
+});
+
+var pageView = function(){
+    var query = $('#query').val();
+	var url = API_URL.replace('{query}',query).replace('{callback}','?').replace('{pageno}',pageno);
+
+    $.ajax(url,{
+		cache : true,
+		dataType : 'jsonp',
+		complete  :function(jqXHR, status){
+			var data = jqXHR.responseJSON;
+			var items = data.channel.item;
+            var list = [];
+			for(var i=0 ; i< items.length; i++){
+				console.log(items[i].title);
+                list.push("<li><a href='"+items[i].link+"'>"+items[i].title.replace(/&lt;/g , '<').replace(/&gt;/g , '>'));
+			}
+            $('#result').append("<ul>" + list.join("") + "</ul>");
+		}
+	});
+	return false;
+};
